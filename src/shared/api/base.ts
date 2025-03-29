898238255fee73e910b2f275f1aebfaf56666906
shaha <shahaunarov7@gmail.com>
2025-03-29 16:18:16 +0500

    shaha

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -1,4 +1,4 @@
-export const API_URL_DOC = `http://26.12.122.72:8000/`
+export const API_URL_DOC = `http://26.253.30.50:8000/`
 // export const API_URL_DOC = `https://ntt.avotra.ru/`
 // export const API_URL_DOC = `http://176.221.28.164/`
 // export const API_URL_DOC = `http://26.12.122.72/`
