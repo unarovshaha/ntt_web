0ea60b472b6a8826f2112ef1ef2b157ed2e03bb1
null <raymuruvsanomuru@gmail.com>
2025-04-03 22:26:59 +0500

    gfhgfgh

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -3,7 +3,6 @@ export const API_URL_DOC = `https://api.xususiytalim.uz/`
 // export const API_URL_DOC = `http://26.12.122.72:8000/`
 
 
-
 // export const API_URL_DOC_IMG = `http://192.168.1.112:8001`
 // export const API_URL_DOC_IMG = `http://26.12.122.72:8000`
 // export const API_URL_DOC_IMG = `http://192.168.1.15:8000`
