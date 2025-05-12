092eea4d3a442964ccadf074b3a6ad3f7ebef9ff
ikromovvv <isardor859@gmail.com>
2025-05-12 21:39:23 +0300

    jlj

--- src/entities/newProfile/ui/newProfileGallery/newProfileGallery.tsx
+++ src/entities/newProfile/ui/newProfileGallery/newProfileGallery.tsx
@@ -1,12 +1,5 @@
-import React, {useState} from 'react';
-
-import cls from "./newProfileGallery.module.sass";
-import image from "shared/assets/images/Rectangle 640.png";
-import {useSelector} from "react-redux";
-import {getHomeProfileGallery} from "entities/home/model/selector/homeSelector";
-import {API_URL, API_URL_DOC} from "shared/api/base";
-import {c} from "framer-motion/dist/types.d-6pKw1mTI";
-import {NewProfilePersonal} from "entities/newProfile/ui/newProfilePersonal/newProfilePersonal";
+import React, { useState } from 'react';
+import cls from './newProfileGallery.module.sass';
 
 interface ImageItem {
     id: number;
@@ -17,36 +10,42 @@ interface GalleryCarouselProps {
     images: ImageItem[];
     apiUrl: string;
 }
+
 export const NewProfileGallery = ({ images, apiUrl }: GalleryCarouselProps) => {
     const [current, setCurrent] = useState(0);
 
-
     const nextSlide = () => {
-        setCurrent((prev) => (prev + 1) % images.length);
+        setCurrent((prev) => (prev + 2) % images.length);
     };
 
     const prevSlide = () => {
-        setCurrent((prev) => (prev - 1 + images.length) % images.length);
+        setCurrent((prev) => (prev - 2 + images.length) % images.length);
     };
 
+    // 2 ta rasm olish
+    const visibleImages = [
+        images[current],
+        images[(current + 1) % images.length] // keyingisi bo'lmasa 0-ga o'tadi
+    ];
 
     return (
-
         <div className={cls.carousel}>
             <button onClick={prevSlide} className={cls.arrow}>‹</button>
 
             <div className={cls.imageWrapper}>
-                {images.map((img, index) => (
-                    <img
-                        key={img.id}
-                        src={`${apiUrl}${img.file}`}
-                        alt={`image-${img.id}`}
-                        className={`${cls.image} ${index === current ? cls.active : ''}`}
-                    />
-                ))}
+                {visibleImages.map((img) =>
+                    img ? (
+                        <img
+                            key={img.id}
+                            src={`${apiUrl}${img.file}`}
+                            alt={`image-${img.id}`}
+                            className={cls.image}
+                        />
+                    ) : null
+                )}
             </div>
 
             <button onClick={nextSlide} className={cls.arrow}>›</button>
         </div>
     );
-}
+};
